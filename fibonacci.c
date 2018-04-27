49aaa1d57bbc180560d55489e1c09c1e431370e3
ari961 <34174185+ari961@users.noreply.github.com>
2018-04-27 20:16:49 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,22 @@
+//This Source code is perfectly runs on Turbo C++
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	int a=-1,b=1,n,c,i;
+	clrscr();
+	printf("\t\t\t-----:Fibonacci Series:-----");
+	printf("\n\nEnter the limitation of the series:: ");
+	scanf("%d",&n);
+	printf("\nThe Fibonacci series is:-\n\n");
+	for(i=0;i<=n;i++)
+	{
+		c=a+b;
+		printf(" %d,",c);
+		a=b;
+		b=c;
+	}
+	printf("\n\npress any key to exit");
+	getch();
+}
