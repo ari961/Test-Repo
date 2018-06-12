21dc3959a852f72943c124e76f03847012045183
ari961 <arijit.roychowdhury.961@gmail.com>
2018-06-12 12:44:49 +0530

    new commit

--- fibonacci.c
+++ fibonacci.c
@@ -1,4 +1,5 @@
 //This Source code is perfectly runs on Turbo C++
+
 #include <stdio.h>
 #include <conio.h>
 
@@ -6,21 +7,23 @@ void main()
 {
 	int a=-1,b=1,n,c,i,sum=0,mul=1;
 	clrscr();
+	
 	printf("\t\t\t-----:Fibonacci Series:-----");
 	printf("\n\nEnter the limitation of the series:: ");
 	scanf("%d",&n);
 	printf("\nThe Fibonacci series is:-\n\n");
-	for(i=0;i<=n;i++)
+	
+	for(i = 0; i <= n; i++)
 	{
-		c=a+b;
-		if(c!=0)     // Series starts with 1
+		c = a + b;
+		if(c != 0)     // Series starts with 1
 		{
 			printf(" %d,",c);
 		}
-		a=b;
-		b=c;
-		sum+=c;
-		mul*=c;
+		a = b;
+		b = c;
+		sum += c;
+		mul *= c;
 	}
 	printf("\n\nThe Sum of the Series is :: %d",sum);
 	printf("\n\nThe Multiplication of the Series is :: %d",mul);
